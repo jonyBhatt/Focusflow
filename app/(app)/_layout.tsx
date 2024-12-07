7ac518da6ce815e77222a675ad7be4fdcfe41fee
jony <jonybhattcharjee886@gmail.com>
2024-12-07 17:52:16 +0600

    Font added

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,5 +1,38 @@
+import { useEffect } from "react";
 import { Stack } from "expo-router";
+import {
+  PlayfairDisplaySC_700Bold as PlayBold,
+  PlayfairDisplaySC_900Black as PlayBlack,
+} from "@expo-google-fonts/playfair-display-sc";
+import {
+  MontserratAlternates_300Light as MontLight,
+  MontserratAlternates_400Regular as MontRegular,
+  MontserratAlternates_500Medium as MontMedium,
+  MontserratAlternates_600SemiBold as MontSemi,
+  MontserratAlternates_700Bold as MontBold,
+  useFonts,
+} from "@expo-google-fonts/montserrat-alternates";
+import * as SplashScreen from "expo-splash-screen";
+
 export default function Layout() {
+  const [loaded, error] = useFonts({
+    MontLight,
+    MontRegular,
+    MontMedium,
+    MontSemi,
+    MontBold,
+    PlayBold,
+    PlayBlack,
+  });
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <Stack>
       <Stack.Screen
